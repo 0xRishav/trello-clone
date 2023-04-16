5543db436d1872ab759bad62946202db41aab92c
Rishav Bharti <rishav.bharti@omnycomm.com>
2023-04-16 15:12:07 +0530

    add dashboard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import Dashboard from './components/Dashboard/Dashboard';
+import { useSelector } from 'react-redux';
+import Project from './components/Project/Project';
 
 function App() {
+  const { isAuthenticated } = useSelector((state) => state.auth);
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/project/:projectId"
+            element={isAuthenticated ? <Project /> : <Navigate to="/login" />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import styles from './Dashboard.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addProject } from '../../state/reducers/project';
+import ProjectCard from '../ProjectCard/ProjectCard';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { projects } = useSelector((state) => state.project);
+  const dispatch = useDispatch();
+  const handleAddProjectClick = () => {
+    console.log('Add project button clicked');
+    dispatch(addProject());
+  };
+
+  return (
+    <div className={styles.dashboard}>
+      {projects.map((project) => (
+        <ProjectCard {...project} />
+      ))}
+      <div className={styles.add_project_btn} onClick={handleAddProjectClick}>
+        <span className={styles.add_project_icon}>+</span>
+        <span className={styles.add_project_text}>
+          Click to add a new project
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
